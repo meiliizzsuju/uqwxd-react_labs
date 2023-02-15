146c117878568761016ec80a254750cd188d3ed5
TichaTin <ticha.tin@gmail.com>
2023-02-15 13:47:20 +1100

    Added counter using Redux

--- react-redux-master/src/index.js
+++ react-redux-master/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
 
-import {createStore} from 'redux';
+import {legacy_createStore as createStore} from 'redux';
 import {Provider} from 'react-redux'
 import myReducers from './reducers'
 
